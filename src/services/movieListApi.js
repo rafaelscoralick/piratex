6f740eb8f3dd25aaefd3450d93fd039652219663
Rafael Scoralick <rafael.scoralick@agencian1.com.br>
2020-01-20 13:55:52 -0300

    ajuste na api para paginação

--- src/services/movieListApi.js
+++ src/services/movieListApi.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 const baseUrl = "https://jsonmock.hackerrank.com/api/movies/search/";
  
-export default async function fetchData(search = ""){
+export default async function fetchData(search = "", page){
     return axios.get(baseUrl, {
-        params: { Title: search } 
+        params: { 
+            Title: search,
+            page: page,
+            per_page: 10
+    } 
     }).then( (movies)=>{
         return movies.data.data
     } )
