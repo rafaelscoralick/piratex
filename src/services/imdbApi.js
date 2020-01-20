0be4f843e39fee43ccb772de34911b291c0de92b
Rafael Scoralick <rafael.scoralick@agencian1.com.br>
2020-01-20 19:31:05 -0300

    ajuste na api

--- src/services/imdbApi.js
+++ src/services/imdbApi.js
@@ -5,7 +5,7 @@ key 1 = 4b6f9272
 key 2 = 511c037a
 */
 
-const imdbAPI = "http://omdbapi.com/?apikey=4b6f9272";
+const imdbAPI = "http://omdbapi.com/?apikey=99b8ea2c";
  
 
 export default async function fetchData(imdbID){
@@ -19,11 +19,18 @@ export default async function fetchData(imdbID){
         console.log("in cache",inCache);
         return inCache;
     }
-    return axios.get(imdbAPI, {
-        params: { i: imdbID } 
-    }).then( (movie)=>{
-        console.log("no cache", movie.data);
-        sessionStorage.setItem("inCache", JSON.stringify([...store, movie.data]) )
-        return movie.data
-    } )
+
+    try {
+        return axios.get(imdbAPI, {
+            params: { i: imdbID } 
+        }).then( (movie)=>{
+            console.log("no cache", movie.data);
+            sessionStorage.setItem("inCache", JSON.stringify([...store, movie.data]) )
+            return movie.data
+        } )
+    } catch (error) {
+        console.log("erro 400!!!, acabou o request");
+        
+    }
+
 }
