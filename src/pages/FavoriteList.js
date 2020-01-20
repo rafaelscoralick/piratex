3a39bc1789b994a42129b9425e9557a122c8a1af
Rafael Scoralick <rafael.scoralick@agencian1.com.br>
2020-01-20 13:55:28 -0300

    lista de favoritos

---
+++ src/pages/FavoriteList.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function FavoriteList() {
+
+  return (
+    <div className="App details">
+        <p>favoritos</p>
+    </div>
+  );
+}
+
+export default FavoriteList;
